375b749534524dfed97f61dcf1deb699eeba3390
samuelfischerramos <sfi18701@gmail.com>
2024-12-05 11:26:50 -0300

    aula02.

---
+++ AULA02/aula02.py
@@ -0,0 +1,11 @@
+print ('good morning')
+
+nome_usuario = 'cristiano ronaldo'# tipo -> nome do papai cirs
+idade_usuario = 39 # tipo-> int = integer
+altura_usuario = 1.87 # tipo -> float = flutuante 
+maior_idade = True # tipo -> bool = boolean
+
+print ('Seja bem-vindo', nome_usuario)
+print('sua idade é', idade_usuario, 'anos')
+print ('você tem',altura_usuario, 'altura')
+print ('você é', maior_idade)
